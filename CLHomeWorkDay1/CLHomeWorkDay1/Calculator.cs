850291d6518f50602f81af4fc55062e854bcb01d
levi <a991002@ccm3s.com>
2016-07-07 08:41:43 +0800

    調整程式碼樣式

--- CLHomeWorkDay1/CLHomeWorkDay1/Calculator.cs
+++ CLHomeWorkDay1/CLHomeWorkDay1/Calculator.cs
@@ -7,8 +7,8 @@
     public class Calculator
     {
         public IEnumerable<int> GroupSum(int groupByCount , List<IProduct> data , Func<IProduct , int> value)
-        {
-            var count = 0;
+        {  // 這段參考同學寫法
+            int count = 0;
             while (count <= data.Count()) {
                 yield return data.Skip(count).Take(groupByCount).Sum(value);
                 count += groupByCount;
--- CLHomeWorkDay1/CLHomeWorkDay1/Product.cs
+++ CLHomeWorkDay1/CLHomeWorkDay1/Product.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CLHomeWorkDay1
+﻿namespace CLHomeWorkDay1
 {
     public interface IProduct
     {
-         int Id { get; set; }
-         int Cost { get; set; }
-         int Revenue { get; set; }
-         int SellPrice { get; set; }
+        int Id { get; set; }
+        int Cost { get; set; }
+        int Revenue { get; set; }
+        int SellPrice { get; set; }
     }
 
-    public class Product: IProduct
+    public class Product : IProduct
     {
         public int Id { get; set; }
         public int Cost { get; set; }
